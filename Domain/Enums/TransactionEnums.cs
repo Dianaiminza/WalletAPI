cc951c179817bd04e6429ab02f6079f90a43151e
Captain <diana.iminza@ronforddigital.com>
2024-06-20 16:39:17 +0300

    initial

---
+++ Domain/Enums/TransactionEnums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public class TransactionEnums
+    {
+        public enum Categories
+        {
+            None = 0,
+            Transport = 1,
+            Food=2,
+            Other=3
+        }
+        public enum ModeOfPayments
+        {
+            None = 0,
+            Mpesa = 1,
+            Bank=2,
+            Cash=3
+        }
+    }
+}
---
+++ Infrastructure/Context/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbContextOptions<DatabaseContext> Options { get; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+              : base(options)
+        {
+           
+            Options = options;
+        }
+    }
+}
